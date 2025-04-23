73dd9ffcde59277d40f92c4619991c6d53adc57a
장연식 <sseregit@knowwheresoft.com>
2025-04-23 16:35:37 +0900

    Flag를 통해 확장성있게 환경변수 관리하기

--- rpc-server/main.go
+++ rpc-server/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"rpc-server/config"
+)
 
+var configFlag = flag.String("config", "./config.toml", "config path")
+
+func main() {
+	flag.Parse()
+
+	config.NewConfig(*configFlag)
 }
