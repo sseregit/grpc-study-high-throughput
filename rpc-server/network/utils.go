fc8944bcbe9955f3b85cd3dd1a1df90af034031c
장연식 <sseregit@knowwheresoft.com>
2025-04-25 09:45:03 +0900

    PasetoToken 검증을 위한 미들웨어 작성하기

---
+++ rpc-server/network/utils.go
@@ -0,0 +1,38 @@
+package network
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
+
+func (n *Network) verifyLogin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		t := getAuthToken(c)
+
+		if t == "" {
+			c.JSON(http.StatusUnauthorized, nil)
+			c.Abort()
+		} else {
+			if _, err := n.gRPCClient.VerifyAuth(t); err != nil {
+				c.JSON(http.StatusUnauthorized, err)
+				c.Abort()
+			} else {
+				c.Next()
+			}
+		}
+
+	}
+}
+
+func getAuthToken(c *gin.Context) string {
+	var token string
+
+	authToken := c.Request.Header.Get("Authorization")
+	authSliced := strings.Split(authToken, " ")
+	if len(authSliced) > 1 {
+		token = authSliced[1]
+	}
+
+	return token
+}
